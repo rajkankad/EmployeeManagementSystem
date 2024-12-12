57de5ca569ec2170ba717abc881fc3cf2c0ac843
rajkankad <rajkankad@gmail.com>
2024-12-12 12:20:06 +0530

    third commit

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,25 +1,25 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
+import React, { createContext, useEffect, useState } from "react";
+import { getLocalStorage, setLocalStorage } from "../utils/localStorage";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [userData, setUserData] = useState(null)
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        // Only set initial localStorage if userData is null
-        if (!getLocalStorage()) {
-            setLocalStorage()
-        }
-        const {employees} = getLocalStorage()
-        setUserData(employees)
-    }, [])
+  useEffect(() => {
+    // Only set initial localStorage if userData is null
+    if (!getLocalStorage()) {
+      setLocalStorage();
+    }
+    const { employees, admin } = getLocalStorage();
+    setUserData({ employees, admin });
+  }, []);
 
-    return (
-        <AuthContext.Provider value={[userData, setUserData]}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={[userData, setUserData]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
