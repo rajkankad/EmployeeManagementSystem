ba7d959eb53ed55364317ce122c3ef25093b7906
rajkankad <rajkankad@gmail.com>
2024-12-12 12:14:24 +0530

    second commit

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -4,24 +4,21 @@ import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
-    // localStorage.clear()
-
     const [userData, setUserData] = useState(null)
 
     useEffect(() => {
-        setLocalStorage()
+        // Only set initial localStorage if userData is null
+        if (!getLocalStorage()) {
+            setLocalStorage()
+        }
         const {employees} = getLocalStorage()
         setUserData(employees)
     }, [])
-    
-    
 
     return (
-        <div>
-            <AuthContext.Provider value={[userData,setUserData]}>
-                {children}
-            </AuthContext.Provider>
-        </div>
+        <AuthContext.Provider value={[userData, setUserData]}>
+            {children}
+        </AuthContext.Provider>
     )
 }
 
